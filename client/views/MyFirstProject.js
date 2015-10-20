6b0abee309dbd3272420a56e8be7e13921cba160
Tyler <mistaken3lf@gmail.com>
2015-10-20 01:36:21 +0000

    Messing with layouts and sweet alerts

---
+++ client/views/MyFirstProject.js
@@ -0,0 +1,17 @@
+Template.content.events({
+  "click .project1": function () {
+    swal("Nice, you clicked the first card button");
+  },
+
+  "click .project2": function () {
+    swal("Nice, you clicked the second card button");
+  },
+
+  "click .project3": function () {
+    swal("Nice, you clicked the third card button");
+  },
+
+  "click .project4": function () {
+    swal("Nice, you clicked the fourth card button");
+  },
+});
